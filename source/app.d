70763db1bdcd3c149f8fa375616eeec9a77d6503
Alexander Milushev <zunkree@gmail.com>
2015-08-02 23:04:48 +0300

    Rasterization of triangles and removing rear faces

--- source/app.d
+++ source/app.d
@@ -3,10 +3,12 @@ import std.math;
 import std.stdio;
 import std.algorithm;
 import wavefront.obj;
+import core.stdc.stdlib: rand;
 
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 const int width  = 2048;
 const int height = 2048;
 Model model;
@@ -44,6 +46,50 @@ void line(int x0, int y0, int x1, int y1, TGAImage image, TGAColor color) {
 }
 
 
+void line(Vec2i p0, Vec2i p1, TGAImage image, TGAColor color) {
+	bool steep = false;
+	if (abs(p0.x-p1.x)<abs(p0.y-p1.y)) {
+		swap(p0.x, p0.y);
+		swap(p1.x, p1.y);
+		steep = true;
+	}
+	if (p0.x>p1.x) {
+		swap(p0, p1);
+	}
+	
+	for (int x=p0.x; x<=p1.x; x++) {
+		float t = (x-p0.x)/cast(float)(p1.x-p0.x);
+		int y = cast(int)(p0.y*(1.-t) + p1.y*t);
+		if (steep) {
+			image.set(y, x, color);
+		} else {
+			image.set(x, y, color);
+		}
+	}
+}
+
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage image, TGAColor color) {
+	if (t0.y>t1.y) swap(t0, t1);
+	if (t0.y>t2.y) swap(t0, t2);
+	if (t1.y>t2.y) swap(t1, t2);
+
+	auto total_height = t2.y-t0.y;
+	for (int i=0; i<total_height; i++) {
+		auto second_half = i>t1.y-t0.y || t1.y==t0.y;
+		auto segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
+		auto alpha = cast(float)i/total_height;
+		auto beta  = cast(float)(i-(second_half ? t1.y-t0.y : 0))/segment_height; // be careful: with above conditions no division by zero here
+		Vec2i A =               t0 + (t2-t0)*alpha;
+		Vec2i B = second_half ? t1 + (t2-t1)*beta : t0 + (t1-t0)*beta;
+		if (A.x>B.x) swap(A, B);
+		for (int j=A.x; j<=B.x; j++) {
+			image.set(j, t0.y+i, color); // attention, due to int casts t0.y+i != A.y
+		}
+	}
+}
+
+
 int main(string[] args)
 {
 	if (args.length == 2){
@@ -52,19 +98,23 @@ int main(string[] args)
 	else {
 		model = new Model("obj/african_head.obj");
 	}
-
+	
 	auto image = new TGAImage(width, height, TGAImage.Format.RGB);
-	for (int i=0; i<model.nfaces; i++) {
-		auto face = model.face(i);
+	auto light_dir = Vec3f(0,0,-1);
+	foreach (face; model.faces.sort!("a[2]>b[2]")) {
+		Vec2i[3] screen_coords;
+		Vec3f[3] world_coords;
 		for (int j=0; j<3; j++) {
-			Vec3f v0 = model.vert(face[j]);
-			Vec3f v1 = model.vert(face[(j+1)%3]);
-			int x0 = cast(int)((v0.x+1.)*width/2.);
-			int y0 = cast(int)((v0.y+1.)*height/2.);
-			int x1 = cast(int)((v1.x+1.)*width/2.);
-			int y1 = cast(int)((v1.y+1.)*height/2.);
-			line(x0, y0, x1, y1, image, white);
+			Vec3f v = model.vert(face[j]);
+			screen_coords[j] = Vec2i(cast(int)((v.x+1.)*width/2.), cast(int)((v.y+1.)*height/2.));
+			world_coords[j] = v;
 		}
+		auto n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+		n.normalize;
+		auto intensity = n*light_dir;
+		if (intensity > 0)
+			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image,
+				     TGAColor(cast(ubyte)(intensity*255), cast(ubyte)(intensity*255), cast(ubyte)(intensity*255), 255));
 	}
 	image.flip_vertically();
 	image.write_tga_file("output.tga");
